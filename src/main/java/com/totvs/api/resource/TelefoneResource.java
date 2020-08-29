5900a615df05519de6ee1e5426c0b552ed780f30
Danilo Duarte <danilo.duarte8@hotmail.com>
2020-08-29 00:08:01 -0300

    feat: update and delete telefone

--- src/main/java/com/totvs/api/resource/TelefoneResource.java
+++ src/main/java/com/totvs/api/resource/TelefoneResource.java
@@ -5,8 +5,10 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,5 +44,38 @@ public class TelefoneResource {
 		Telefone newTelefone = telefoneRepository.save(telefone);
 		
 		return new ResponseEntity<>(newTelefone, HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/telefone/{id}")
+	public ResponseEntity<Object> update(@PathVariable(value="id") long id, @RequestBody Telefone telefone) {
+		Optional<Telefone> telefoneAntigo = telefoneRepository.findById(id);
+		
+		if(!telefoneAntigo.isPresent()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+		Telefone telefoneNovo = telefoneAntigo.get();
+		
+		telefoneNovo.setCodigoPais(telefone.getCodigoPais());
+		telefoneNovo.setDdd(telefone.getDdd());
+		telefoneNovo.setNumero(telefone.getNumero());
+		telefoneNovo.setTipo(telefone.getTipo());
+		
+		Telefone updated = telefoneRepository.save(telefoneNovo);
+		
+		return new ResponseEntity<>(updated, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/telefone/{id}")
+	public ResponseEntity<Object> destroy(@PathVariable(value="id") long id) {
+		Optional<Telefone> telefone = telefoneRepository.findById(id);
+		
+		if(!telefone.isPresent()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+		telefoneRepository.delete(telefone.get());
+		
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 }
