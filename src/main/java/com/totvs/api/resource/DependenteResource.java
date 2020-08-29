58a1095d36215dc7da9db8cdde34f984bbef923f
Danilo Duarte <danilo.duarte8@hotmail.com>
2020-08-29 00:07:48 -0300

    feat: update and delete dependente

--- src/main/java/com/totvs/api/resource/DependenteResource.java
+++ src/main/java/com/totvs/api/resource/DependenteResource.java
@@ -5,8 +5,10 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,5 +44,36 @@ public class DependenteResource {
 		Dependente novoDependente = dependenteRepository.save(dependente);
 		
 		return new ResponseEntity<>(novoDependente, HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/dependente/{id}")
+	public ResponseEntity<Object> update(@PathVariable(value="id") long id, @RequestBody Dependente dependente) {
+		Optional<Dependente> dependenteAntigo = dependenteRepository.findById(id);
+		
+		if(!dependenteAntigo.isPresent()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+		Dependente dependenteNovo = dependenteAntigo.get();
+		
+		dependenteNovo.setNome(dependente.getNome());
+		dependenteNovo.setTipo(dependente.getTipo());
+		
+		Dependente updated = dependenteRepository.save(dependenteNovo);
+		
+		return new ResponseEntity<>(updated, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/dependente/{id}")
+	public ResponseEntity<Object> destroy(@PathVariable(value="id") long id) {
+		Optional<Dependente> dependente = dependenteRepository.findById(id);
+		
+		if(!dependente.isPresent()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+		dependenteRepository.delete(dependente.get());
+		
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 }
